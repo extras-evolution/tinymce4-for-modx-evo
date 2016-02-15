2307373e1cf1ecd88357a95aac9d042d788e07ab
yamamoto <yamamoto@kyms.jp>
2016-02-15 19:07:13 +0900

    Refactor

--- assets/plugins/tinymce4/plugin.tinymce.php
+++ assets/plugins/tinymce4/plugin.tinymce.php
@@ -6,11 +6,8 @@ global $usersettings,$settings;
 
 // Set path and base setting variables
 
-$params['mce_path']         = $mce_path;
-$params['mce_url']          = $mce_url;
-
 $plugin_dir = 'tinymce';
-include_once("{$mce_path}plugin.class.inc.php");
+include_once(dirname(__FILE__).'/plugin.class.inc.php');
 
 $mce4 = new TinyMCE4();
 
@@ -21,20 +18,15 @@ switch ($e->name)
 	case "OnRichTextEditorRegister": // register only for backend
 		$e->output('TinyMCE4');
 		break;
-
 	case "OnRichTextEditorInit":
 		if($editor!=='TinyMCE4') return;
-		
 		$script = $mce4->get_mce_script();
 		$e->output($script);
 		break;
-
 	case "OnInterfaceSettingsRender":
 //		$html = $mce4->get_mce_settings();
 //		$e->output($html);
 		break;
-
    default :
       return; // stop here - this is very important. 
-      break; 
 }
