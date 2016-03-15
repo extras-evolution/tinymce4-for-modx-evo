61d39a5e90ebe156bbf9df6c60266cded74430d7
Deesen <dh@fuseit.de>
2016-03-15 10:35:12 +0100

    Bugfix: Force include() - to pass right __FILE__ when pasting code of plugin.tinymce.php manually - required to show settings in modx-configuration

--- assets/plugins/tinymce4/plugin.tinymce.php
+++ assets/plugins/tinymce4/plugin.tinymce.php
@@ -15,68 +15,10 @@
  *
  * @author Yama / updated: 2015-01-16
  * @author Dmi3yy / updated: 2016-01-07
- * @author Deesen / updated: 2016-03-13
+ * @author Deesen / updated: 2016-03-15
  *
  * Latest Updates / Issues on Github : https://github.com/extras-evolution/tinymce4-for-modx-evo
  */
 if (!defined('MODX_BASE_PATH')) { die('What are you doing? Get out of here!'); }
 
-// Init
-include_once(MODX_BASE_PATH."assets/lib/class.modxRTEbridge.php");
-$rte = new modxRTEbridge('tinymce4', __FILE__, $options);
-$rte->setDebug(false);  // true or 'full' for Debug-Infos in HTML-comments
-
-// Overwrite theme
-// $rte->force('width',          '75%', 'string' );                               // Overwrite width parameter
-// $rte->force('height',         isset($height) ? $height : '400px', 'string' );  // Get/set height from plugin-configuration
-// $rte->force('height',         NULL );                                          // Removes "height" completely from editor-init
-
-
-// Internal Stuff - Don´t touch!
-$showSettingsInterface = true;  // Show/Hide interface in Modx- / user-configuration
-$editorLabel = $rte->pluginParams['editorLabel'];
-$editableClass = !empty( $rte->pluginParams['editableClass'] ) ? $rte->pluginParams['editableClass'] : 'editable';
-
-$e = &$modx->event;
-switch ($e->name) {
-    // register for manager
-    case "OnRichTextEditorRegister":
-        $e->output($editorLabel);
-        break;
-
-    // render script for JS-initialization
-    case "OnRichTextEditorInit":
-        if ($editor === $editorLabel) {
-            $script = $rte->getEditorScript();
-            $e->output($script);
-        };
-        break;
-
-    // render script for Frontend JS-initialization (Inline-Mode)
-    case "OnWebPagePrerender":
-        if($inlineMode == 'enabled') {
-            $rte->set('inline', true, 'bool'); // https://www.tinymce.com/docs/configure/editor-appearance/#inline
-            $rte->setPluginParam('elements', $editableClass);  // Set missing plugin-parameter manually for Frontend
-            $rte->addEditorScriptToBody();
-        }
-        break;
-
-    // Avoid breaking content / parsing of Modx-placeholders when editing (Inline-Mode)
-    case "OnLoadWebDocument":
-        if($inlineMode == 'enabled') {
-            $rte->protectModxPhs($editableIds);
-        }
-        break;
-
-    // render Modx- / User-configuration settings-list
-    case "OnInterfaceSettingsRender":
-        if( $showSettingsInterface === true ) {
-            $html = $rte->getModxSettings();
-            $e->output($html);
-        };
-        break;
-
-    default :
-        return; // important! stop here!
-        break;
-}
+include(MODX_BASE_PATH."assets/plugins/tinymce4/plugin.tinymce.inc.php");
